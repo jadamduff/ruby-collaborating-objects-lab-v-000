b269fa97b91fcae371648ede9cb91d7ad2ecd703
Adam Duff <j.adam.duff@gmail.com>
2018-08-16 03:43:43 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,4 +10,17 @@ class MP3Importer
     @files.delete_if {|file| file == "." || file == ".."}
     @files
   end
+
+  def import
+    self.files.each do |file|
+      new_file = file.split(" - ")
+      artist = new_file[0]
+      song = new_file[1]
+
+      new_artist = Artist.find_or_create_by_name(artist)
+      new_song = Song.new(song)
+      new_artist.songs << new_song
+      new_song.artist = new_artist
+    end
+  end
 end
