7cc1a6166fc19bf4afc147f1a6ddfdfe9ac9cfc5
Adam Duff <j.adam.duff@gmail.com>
2018-08-16 03:25:37 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,11 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path :files
+
   def initialize(filepath)
     @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
   end
 end
