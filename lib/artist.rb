ab30b670e5d3e8db6d6255e70527b7748e795ca9
Adam Duff <j.adam.duff@gmail.com>
2018-08-16 03:00:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     self.all.each do |artist|
-      if artist[:name] == name
+      if artist.name == name
         return artist
       end
     end
