153deafb0128af2c01b7b00a7c1d8ceb45b4230c
Adam Duff <j.adam.duff@gmail.com>
2018-08-16 03:48:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,5 +7,12 @@ class Song
 
   def self.new_by_filename(file)
     new_file = @file.split(" - ")
+    artist = new_file[0]
+    song = new_file[1]
+
+    new_song = Song.new(song)
+    new_artist = Artist.find_or_create_by_name(artist)
+    new_song.artist = new_artist
+    new_artist.songs << new_song
   end
 end
